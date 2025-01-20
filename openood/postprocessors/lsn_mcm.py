f7d1aa96e8dd40c5c1319c5d1dc97cfde93dba1c
Vishnu <vishnu.thirtha.haresamudram@cariad.technology>
2025-01-20 09:35:29 +0000

    updated mcm for lsn

--- openood/postprocessors/lsn_mcm.py
+++ openood/postprocessors/lsn_mcm.py
@@ -18,18 +18,18 @@ class LSNPostprocessor(BasePostprocessor):
         logits, [outputs_yes, outputs_no1, outputs_no2, outputs_no3] = net(data)
     
         similarity_yes = F.softmax(outputs_yes, dim=1)
-        similarity_no1 = F.softmax((1-outputs_no1)/5 , dim=1)
-        similarity_no2 = F.softmax((1-outputs_no2)/5 , dim=1)
-        similarity_no3 = F.softmax((1-outputs_no3)/5 , dim=1)
+        #similarity_no1 = F.softmax((1-outputs_no1)/5 , dim=1)
+        #similarity_no2 = F.softmax((1-outputs_no2)/5 , dim=1)
+        #similarity_no3 = F.softmax((1-outputs_no3)/5 , dim=1)
         
         max_out_yes, index_out_yes = torch.max(similarity_yes,dim = 1)
-        max_out_no1, index_out_no = torch.max(similarity_no1,dim = 1)
-        max_out_no2, index_out_no = torch.max(similarity_no2,dim = 1)
-        max_out_no3, index_out_no = torch.max(similarity_no3,dim = 1)
-        max_out_no = (max_out_no1 + max_out_no2 + max_out_no3)/3
-        max_out_yes_plus = max_out_yes + max_out_no
+        #max_out_no1, index_out_no = torch.max(similarity_no1,dim = 1)
+        #max_out_no2, index_out_no = torch.max(similarity_no2,dim = 1)
+        #max_out_no3, index_out_no = torch.max(similarity_no3,dim = 1)
+        #max_out_no = (max_out_no1 + max_out_no2 + max_out_no3)/3
+        #max_out_yes_plus = max_out_yes + max_out_no
         
-        return index_out_yes, max_out_yes_plus
+        return index_out_yes, max_out_yes
 
     def set_hyperparam(self, hyperparam: list):
         self.tau = hyperparam[0]
